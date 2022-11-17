c169b8835742a88ded11c082e71c4b99e60690cf
Adina Amzărescu <73987572+adinaamzarescu@users.noreply.github.com>
2022-11-17 15:58:07 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,13 +10,15 @@ seq_times=()
 par_times=()
 seq_runs=0
 
+# afiseaza scorul final
 function show_score {
 	echo ""
-	echo "Score scalability: $scalability/70"
-	echo "Score correctness: $correctness/50"
-	echo "Score:       $((correctness + scalability))/120"
+	echo "Scor scalabilitate: $scalability/70"
+	echo "Scor corectitudine: $correctness/50"
+	echo "Scor total:       $((correctness + scalability))/120"
 }
 
+# se compara output-urile din doua directoare (parametri: director1 director2 exponent)
 function compare_outputs {
 	ok=0
 
@@ -36,21 +38,22 @@ function compare_outputs {
 	then
 		correct=$((correct+1))
 	else
-		echo "W: Differences between $1 and $2"
+		echo "W: Exista diferente intre output-urile din directorul $1 si cele din directorul $2"
 		correct_scalability=$((correct_scalability-1))
 	fi
 }
 
+# se ruleaza o comanda  (parametru: comanda)
 function run {
-	{ time -p sh -c "timeout 120 $1"; } &> time.txt
+	{ time -p sh -c "timeout 200 $1"; } &> time.txt
 	ret=$?
 
 	if [ $ret == 124 ]
 	then
-		echo "W: The program took more than 120 s"
+		echo "W: Programul a durat mai mult de 200 s"
 	elif [ $ret != 0 ]
 	then
-		echo "W: Couldn't execute"
+		echo "W: Rularea nu s-a putut executa cu succes"
 		cat time.txt | sed '$d' | sed '$d' | sed '$d'
 	fi
 
@@ -62,20 +65,21 @@ function run {
 	seq_runs=$((seq_runs+1))
 }
 
+# se ruleaza o comanda si se masoara timpul (parametru: comanda)
 function run_and_get_time {
-	{ time -p sh -c "timeout 120 $1"; } &> time.txt
+	{ time -p sh -c "timeout 200 $1"; } &> time.txt
 	ret=$?
 
 	if [ $ret == 124 ]
 	then
-		echo "W: The program took more than 120 s"
+		echo "E: Programul a durat mai mult de 200 s"
 		cat time.txt | sed '$d' | sed '$d' | sed '$d'
 		show_score
 		rm -rf test*_sec
 		exit
 	elif [ $ret != 0 ]
 	then
-		echo "W: Couldn't execute"
+		echo "E: Rularea nu s-a putut executa cu succes"
 		cat time.txt | sed '$d' | sed '$d' | sed '$d'
 		show_score
 		rm -rf test*_sec
@@ -92,16 +96,17 @@ function run_and_get_time {
 	seq_runs=$((seq_runs+1))
 }
 
+# se ruleaza si masoara o comanda paralela (parametri: timeout comanda)
 function run_par_and_measure {
 	{ time -p sh -c "timeout $1 $2" ; } &> time.txt
 	ret=$?
 
 	if [ $ret == 124 ]
 	then
-		echo "W: The program took at least 2 seconds more than the secvential implementation."
+		echo "W: Programul a durat cu cel putin 2 secunde in plus fata de implementarea secventiala"
 	elif [ $ret != 0 ]
 	then
-		echo "W: Couldn't execute"
+		echo "W: Rularea nu s-a putut executa cu succes"
 		cat time.txt | sed '$d' | sed '$d' | sed '$d'
 	fi
 
@@ -110,12 +115,13 @@ function run_par_and_measure {
 	rm -rf time.txt
 }
 
+# se compileaza tema
 make clean &> /dev/null
 make build &> build.txt
 
-if [ ! -f a.out ]
+if [ ! -f tema1 ]
 then
-	echo "E: Couldn't compile"
+	echo "E: Nu s-a putut compila tema"
 	cat build.txt
 	show_score
 	rm -rf build.txt
@@ -127,19 +133,21 @@ rm -rf build.txt
 for i in `seq 0 4`
 do
 	echo ""
-	echo "======== Test ${i} ========"
+	echo "======== Testul ${i} ========"
 	echo ""
-	echo "Case M=1 si R=4..."
+	echo "Se ruleaza varianta etalon cu M=1 si R=4..."
 
+	# se creeaza directorul de output secvential
 	mkdir -p test${i}_sec
 
-	run_and_get_time "./a.out 1 4 ./test${i}/test.txt > test${i}_sec/out.txt"
+	run_and_get_time "./tema1 1 4 ./test${i}/test.txt > test${i}_sec/out.txt"
 	mv out*.txt test${i}_sec/ 2>/dev/null
 
-	echo "It took ${seq_times[$i]} seconds"
+	echo "Rularea a durat ${seq_times[$i]} secunde"
 
 	correct_scalability=3
 
+	# se verifica daca rezultatele sunt corecte
 	compare_outputs test${i}_sec test${i} 5
 
 	rm -rf test${i}_sec
@@ -147,15 +155,19 @@ do
 	echo "OK"
 	echo ""
 
+	# se creste valoarea de timeout cu 2 secunde
 	times[$i]=$((times[$i]+2))
 
+	# se creeaza directorul de output paralel
 	mkdir -p test${i}_par
 
+	# se ruleaza implementarea paralela pe 2 si 4 thread-uri
 	for P in 2 4
 	do
-		echo "Case M=$P si R=4..."
+		echo "Se ruleaza varianta cu M=$P si R=4..."
 
-		run_par_and_measure ${times[$i]} "./a.out $P 4 ./test${i}/test.txt > test${i}_par/out.txt"
+		# se masoara timpii paraleli (pentru calculul acceleratiei)
+		run_par_and_measure ${times[$i]} "./tema1 $P 4 ./test${i}/test.txt > test${i}_par/out.txt"
 		mv out*.txt test${i}_par/ 2>/dev/null
 
 		if [ $P == 2 ]
@@ -164,6 +176,8 @@ do
 		else
 			echo "Rularea a durat ${par_times[$i * 2 + 1]} secunde"
 		fi
+
+		# se tine minte daca rezultatul e corect (pentru punctajul de scalabilitate)
 		compare_outputs test${i}_par test${i} 5
 
 		echo "OK"
@@ -174,22 +188,26 @@ do
 
 	if [ $i != 0 ] && [ $i != 4 ]
 	then
+		# se calculeaza acceleratia
 		speedup12=$(echo "${seq_times[$i]}/${par_times[$i * 2]}" | bc -l | xargs printf "%.2f")
 		speedup14=$(echo "${seq_times[$i]}/${par_times[$i * 2 + 1]}" | bc -l | xargs printf "%.2f")
 
+		# acceleratia se considera 0 daca testele de scalabilitate nu sunt corecte
 		if [ $correct_scalability != 3 ]
 		then
 			speedup12=0
 			speedup14=0
-			echo "Wrong results"
+			echo "Testele nu dau rezultate corecte, acceleratia se considera 0"
 		fi
 
-		printf "Acceleration 1-2 Mappers: %0.2f\n" $speedup12
-		printf "Acceleration 1-4 Mappers: %0.2f\n" $speedup14
+		printf "Acceleratie 1-2 Mappers: %0.2f\n" $speedup12
+		printf "Acceleratie 1-4 Mappers: %0.2f\n" $speedup14
 
+		###
 		if [ $i != 1 ]
 		then
-			max=$(echo "${speedup12} > 1.4" | bc -l)
+			# se verifica acceleratia de la secvential la 2 thread-uri
+			max=$(echo "${speedup12} > 1.3" | bc -l)
 			part=$(echo "${speedup12} > 1.1" | bc -l)
 			if [ $max == 1 ]
 			then
@@ -197,23 +215,24 @@ do
 			elif [ $part == 1 ]
 			then
 				scalability=$((scalability+7))
-				echo "W: Acceleration from 1 to 2 Mappers too low"
+				echo "W: Acceleratia de la 1 la 2 Mappers este prea mica (punctaj partial)"
 			else
-				echo "W: Acceleration from 1 to 2 Mappers too low"
+				echo "W: Acceleratia de la 1 la 2 Mappers este prea mica (fara punctaj)"
 			fi
 		fi
 
-		max=$(echo "${speedup14} > 2.1" | bc -l)
-		part=$(echo "${speedup14} > 1.8" | bc -l)
+		# se verifica acceleratia de la secvential la 4 thread-uri
+		max=$(echo "${speedup14} > 1.9" | bc -l)
+		part=$(echo "${speedup14} > 1.5" | bc -l)
 		if [ $max == 1 ]
 		then
 			scalability=$((scalability+14))
 		elif [ $part == 1 ]
 		then
 			scalability=$((scalability+7))
-			echo "W: Acceleration from 1 to 4 Mappers too low"
+			echo "W: Acceleratia de la 1 la 4 Mappers este prea mica (punctaj partial)"
 		else
-			echo "W: Acceleration from 1 to 4 Mappers too low"
+			echo "W: Acceleratia de la 1 la 4 Mappers este prea mica (fara punctaj)"
 		fi
 	fi
 
